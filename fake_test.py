835a966275736a6c7629f3560002eb1a1ca1c671
Harrison King Saturley-Hall <hsaturleyhal@nvidia.com>
2025-03-20 13:16:36 -0400

    only show failing tests

--- fake_test.py
+++ fake_test.py
@@ -2,6 +2,14 @@ import pytest
 
 def test_failing():
     assert False
+    
+def test_failing2():
+    assert False
 
-def test_passing():
+def test_passing1():
+    assert True
+
+def test_passing2():
+    assert True
+def test_passing3():
     assert True
---
+++ fake_test2.py
@@ -0,0 +1,14 @@
+import pytest
+
+def test_failing():
+    assert False
+    
+def test_failing2():
+    assert False
+
+
+def test_failing():
+    assert False
+    
+def test_failing2():
+    pass
